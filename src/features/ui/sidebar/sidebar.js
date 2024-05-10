af34bf957c64f1a378efeec759354addcff75e1c
Mobinul-Sekh <mskmd06@gmail.com>
2024-05-11 01:11:45 +0530

    Good progress has been made on the sidebar.

--- src/features/ui/sidebar/sidebar.js
+++ src/features/ui/sidebar/sidebar.js
@@ -1,29 +1,41 @@
 import './sidebar.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBullseye, faBoltLightning, faStar, faSignOut } from '@fortawesome/free-solid-svg-icons'
+import { faBullseye, faBoltLightning, faStar, faSignOut, faShop } from '@fortawesome/free-solid-svg-icons'
+import { useState } from 'react'
 
 function SideBar() {
+  const [expendSideBar, setExpendSideBar] = useState(false)
+  const [selectedItem, setSelectedItem] = useState('2')
+
+  function handleExpendBehavior() {
+    setExpendSideBar((expendSideBar) => !expendSideBar)
+  }
+  function handleSelectedItem(item) {
+    setSelectedItem(item)
+  }
+
   return (
     <>
-      <div className='sidebar'>
-        <h2 className='brand-name'>ShopSuper</h2>
+      <div className={`sidebar ${expendSideBar ? '' : 'sidebar-shrink'}`} >
+        { expendSideBar && <h2 className='brand-name' onClick={handleExpendBehavior}>ShopSuper</h2> }
+        { !expendSideBar && <FontAwesomeIcon className='brand-logo' onClick={handleExpendBehavior} icon={faShop} size='xl'/> }
         <ul className='sidebar-list'>
-          <li>
+          <li onClick={() => handleSelectedItem('1')} className={selectedItem === '1' ? `select-option ${ expendSideBar ? '' : 'select-option-shrink'}` : ''}>
             <FontAwesomeIcon icon={faBullseye}/>
-            <p>Popular Brands</p>
+            { expendSideBar && <p>Popular Brands</p> }
           </li>
-          <li>
+          <li onClick={() => handleSelectedItem('2')} className={selectedItem === '2' ? `select-option ${ expendSideBar ? '' : 'select-option-shrink'}` : ''}>
             <FontAwesomeIcon icon={faBoltLightning}/>
-            <p>Explore</p>
+            { expendSideBar && <p>Explore</p> }
           </li>
-          <li>
+          <li onClick={() => handleSelectedItem('3')} className={selectedItem === '3' ? `select-option ${ expendSideBar ? '' : 'select-option-shrink'}` : ''}>
             <FontAwesomeIcon icon={faStar}/>
-            <p>Special for you</p>
+            { expendSideBar && <p>Special for you</p> }
           </li>
         </ul>
         <div className='logout-text'>
           <FontAwesomeIcon icon={faSignOut}/>
-          <p>logout</p>
+          { expendSideBar && <p>logout</p> }
         </div>
       </div>
     </>
